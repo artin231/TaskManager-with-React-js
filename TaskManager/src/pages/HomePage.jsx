7de16576b599f3ea71b044317be0305163e8075e
artin231 <leiartrezjah@gmail.com>
2026-08-09 19:38:35 +0330

    starting the main page

--- TaskManager/src/pages/HomePage.jsx
+++ TaskManager/src/pages/HomePage.jsx
@@ -2,10 +2,21 @@ import Header from "../components/Header"
 
 let HomePage = () => {
     return (
-        <>
-        <Header />
-        <h1>home</h1>
-        </>
+        <div className="container">
+            <Header />
+            <div className="TasksBox">
+                <h1>تسک ها</h1>
+                <div className="TaskList">
+                    <div className="task">
+                        <div className="left">
+                        <h1 className="task_title"></h1>
+                        <h1 className="task_status"></h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     )
 }
 
