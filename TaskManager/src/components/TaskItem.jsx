53a70dc7030e6e39c6111ea6353499f787e722f0
artin231 <leiartrezjah@gmail.com>
2026-08-11 15:08:33 +0330

    now you can change the status

--- TaskManager/src/components/TaskItem.jsx
+++ TaskManager/src/components/TaskItem.jsx
@@ -1,15 +1,20 @@
+import { useContext, useState } from "react"
+import { TaskContext } from "../context/TaskContext"
+
 let TaskItem = ({ i, h }) => {
+    let data = useContext(TaskContext);
+
     return (
         <div className="ItemTaskList">
             <div className="status">
-                {i.finised == true ? <> <input type="checkbox" key={h} name="" id="" onClick={() => {}} checked /> <p>انجام شده</p> </> : <><input type="checkbox" onClick={() => {}}  key={h} name="" id="" /> <p>انجام نشده</p> </>}
+                {i.finised == true ? <> <input type="checkbox" key={h} name="" id="" onClick={() => {data.ChangeStatusItem(i.id)}} checked /> <p>انجام شده</p> </> : <><input type="checkbox" onClick={() => {data.ChangeStatusItem(i.id)}} key={h} name="" id=""  /> <p>انجام نشده</p> </>}
 
             </div>
-<div className="right">
-            <h1>{i.name}</h1>
-            <img className="deleteIcon" src="src/assets/red-trash-can-icon.svg" alt="" />
-            <img className="EditIcon" src="src/assets/pencil-icon-113248-512.png" alt="" />
-</div>
+            <div className="right">
+                <h1>{i.name}</h1>
+                <img className="deleteIcon" src="src/assets/red-trash-can-icon.svg" alt="" />
+                <img className="EditIcon" src="src/assets/pencil-icon-113248-512.png" alt="" />
+            </div>
 
 
         </div>
--- TaskManager/src/context/TaskContext.jsx
+++ TaskManager/src/context/TaskContext.jsx
@@ -6,8 +6,20 @@ export let TaskContext = createContext();
 
 export let TaskProviderFunc = ({ children }) => {
     let [ListTask,UpdateListTask] = useState([{id:uuidv4(),name:'برنامه نویسی',finised:false}]);
+
+    let ChangeStatusItem = (Id) => {
+        let updatedList = ListTask.map(i => {
+            if(i.id == Id){
+                i.finised = !i.finised;
+                return i
+            }
+            return i
+        })
+        UpdateListTask(updatedList)
+    };
+
     return (
-        <TaskContext.Provider value={{ListTask,UpdateListTask}}>
+        <TaskContext.Provider value={{ListTask,UpdateListTask,ChangeStatusItem}}>
             {children}
         </TaskContext.Provider>
     )
