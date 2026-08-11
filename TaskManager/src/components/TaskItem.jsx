25dcc516e6aa8d5188b7a3a64382d2b77945ceb7
artin231 <leiartrezjah@gmail.com>
2026-08-11 14:39:13 +0330

    i added a the icons anf its finish in the designing in the css

--- TaskManager/src/components/TaskItem.jsx
+++ TaskManager/src/components/TaskItem.jsx
@@ -2,12 +2,15 @@ let TaskItem = ({ i, h }) => {
     return (
         <div className="ItemTaskList">
             <div className="status">
-                {i.finised == true ? <> <input type="checkbox" key={h} name="" id="" onClick={() => {}} checked /> <p>انجام شده</p> </> : <><input type="checkbox" key={h} name="" id="" /> <p>انجام نشده</p> </>}
+                {i.finised == true ? <> <input type="checkbox" key={h} name="" id="" onClick={() => {}} checked /> <p>انجام شده</p> </> : <><input type="checkbox" onClick={() => {}}  key={h} name="" id="" /> <p>انجام نشده</p> </>}
 
             </div>
-
+<div className="right">
             <h1>{i.name}</h1>
-            <i className="fa fa-trash" aria-hidden="true"></i>
+            <img className="deleteIcon" src="src/assets/red-trash-can-icon.svg" alt="" />
+            <img className="EditIcon" src="src/assets/pencil-icon-113248-512.png" alt="" />
+</div>
+
 
         </div>
     )
